6832c2a638262a7eb0cf80a690904be05a97a679
Sergey <piolersa85@mail.ru>
2021-09-30 19:30:28 +0200

    Sean Allen Alert

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var alertLabel: UILabel!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+//MARK: - Простой Алерт
+    @IBAction func AlertButton(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Внимание!",
+                                      message: "Нажми кнопку для закрытия окна",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+//MARK: - Второй вариант
+
+    
+    @IBAction func alertTwo(_ sender: UIButton) {
+        
+        alertLabel.text = "🧸 Улыбку "
+        alert(title: "Вариант ", message: "Номер Два", style: .alert)
+        
+        
+    }
+    
+    func alert(title: String, message: String, style: UIAlertController.Style){
+        
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: style)
+        
+        
+      
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default) { (action) in
+            
+            let text = alert.textFields?.first
+            self.alertLabel.text! += (text?.text!)! + (" 🧸")
+            
+        })
+        
+        alert.addTextField { textField in }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
 
 
-}
+//MARK: - Третий вариант от SEaN Allen
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
